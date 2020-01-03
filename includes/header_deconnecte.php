75cb5ee34218858f89f45d3792b78cf2a9b49589
FatherKarras333 <florent.victorin@icloud.com>
2020-01-03 13:13:34 +0100

    Création du header utilisateur déconnecté avec le logo centré

---
+++ includes/header_deconnecte.php
@@ -0,0 +1,6 @@
+<!-- Header pour les pages d'inscription et de connexion avec le logo centré -->
+<header class="header_responsive">
+	
+	<p class="logo_align"><img class="logo" src="images/logo100px.png" alt="logo de l'entreprise"></p>
+			
+</header>
