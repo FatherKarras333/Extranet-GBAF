d2188de57b6c8b1a03fb91668d11217cb545ae18
FatherKarras333 <florent.victorin@icloud.com>
2020-01-03 14:00:20 +0100

    Ajout de propriétés CSS et affichage du nom et prénom de l'utilisateur grâce aux variables de session

--- includes/header_connecte.php
+++ includes/header_connecte.php
@@ -4,9 +4,8 @@
 					<p><a href="accueil.php"><img id="logo" src="images/logo100px.png" alt="logo de l'entreprise"></a></p>
 					
 					<div class="icon">
-						<a href="deconnexion.php"><img src="images/deconnexion.png" alt="logo de connexion"></a>
-						<a href="parametres.php" ><img src="images/login_icon.png" alt="logo de connexion"></a>
-						<a href="parametres.php"> Nom Prénom</a>
-								
+						<a href="deconnexion.php"><img src="images/deconnexion.png" alt="logo de connexion" style="width: 28px; margin-right: 10px;"></a>
+						<a href="parametres.php" style="margin-right: 10px;"><img src="images/login_icon.png" alt="logo de connexion"></a>
+						<a href="parametres.php" style="font-size: 1.1em; position: relative; bottom:5px; color: #333;"><?php echo $_SESSION['nom'] ; ?> <?php echo $_SESSION['prenom']; ?></a>			
 					</div>	
 			</header>
