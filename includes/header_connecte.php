964ab8abcc682f3823bff2e76ca6781f0193ad54
FatherKarras333 <florent.victorin@icloud.com>
2020-01-03 12:15:36 +0100

    Création du header lorsque l'utilisateur est connécté

---
+++ includes/header_connecte.php
@@ -0,0 +1,12 @@
+<!-- Header de la page d'acceuil et des pages acteurs -->		
+
+			<header>
+					<p><a href="accueil.php"><img id="logo" src="images/logo100px.png" alt="logo de l'entreprise"></a></p>
+					
+					<div class="icon">
+						<a href="deconnexion.php"><img src="images/deconnexion.png" alt="logo de connexion"></a>
+						<a href="parametres.php" ><img src="images/login_icon.png" alt="logo de connexion"></a>
+						<a href="parametres.php"> Nom Prénom</a>
+								
+					</div>	
+			</header>
