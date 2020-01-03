c40fbddfefa97b888f38f479e37aaa88e1b4a78f
FatherKarras333 <florent.victorin@icloud.com>
2020-01-03 12:20:48 +0100

    Création du footer

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+	
+	<p> |<a href="#" class="texte_footer">| Mentions légales |</a> <a href="mailto:florent.victorin@icloud.com" class="texte_footer"> Contact |</a></p>
+	<p> Site réalisé dans le cadre d'une formation Openclassrooms</p>
+				
+</footer>
