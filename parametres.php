23ba2142da431648821054e7a400e13da4317836
FatherKarras333 <florent.victorin@icloud.com>
2020-01-04 10:25:40 +0100

    Insertion du nouveau nom, prénom, pseudo, mot de passe, question et réponse. Vérifier que le pseudo ne soit pas déjà  pris, que le mot de passe soit correct

--- parametres.php
+++ parametres.php
@@ -1,9 +1,119 @@
 <?php
 session_start();
 
+		try
+		{
+			$bdd = new PDO('mysql:host=localhost;dbname=vifl4713_bdd;charset=utf8','vifl4713_bdd', 'LOCWqSqgX2PduJlbyC'); 
+		}
+		catch (Exception $e)
+		{
+			die('Erreur : ' . $e->getMessage());
+		}
+
 if (isset($_SESSION['id'])) 
 {
+	if (isset($_POST['nom']) AND !empty($_POST['nom']) AND $_POST['nom'] != $_SESSION['nom']) 
+	{
+			$nouveauNom = htmlspecialchars($_POST['nom']);
+			
+			$insererNom = $bdd->prepare('UPDATE members SET nom = :nom WHERE id = :id');
+			$insererNom->execute([':nom'=>$nouveauNom, ':id'=>$_SESSION['id']]);
+			$_SESSION['nom'] = $nouveauNom ;
+			header('Location: accueil.php ');	
+	}
+
+	if (isset($_POST['prenom']) AND !empty($_POST['prenom']) AND $_POST['prenom'] != $_SESSION['prenom']) 
+	{
+			$nouveauPrenom = htmlspecialchars($_POST['prenom']);
+			
+			$insererPrenom = $bdd->prepare('UPDATE members SET prenom = :prenom WHERE id = :id');
+			$insererPrenom->execute([':prenom'=>$nouveauPrenom, ':id'=>$_SESSION['id']]);
+			$_SESSION['prenom'] = $nouveauPrenom ;
+			header('Location: accueil.php ');	
+	}
+	
+	if (isset($_POST['nouveau_pseudo']) AND !empty($_POST['nouveau_pseudo']) AND $_POST['nouveau_pseudo'] != $_SESSION['pseudo']) 
+	{
+			$nouveauPseudo = htmlspecialchars($_POST['nouveau_pseudo']);
+
+			$pseudolength = strlen($nouveauPseudo);		
+			if ($pseudolength <= 30)
+			{
+				$requetePseudo = $bdd->prepare('SELECT * FROM members WHERE pseudo = :pseudo');
+				$requetePseudo->execute(['pseudo'=>$nouveauPseudo]);
+				$pseudoExiste = $requetePseudo->rowCount();
+				if ($pseudoExiste === 0)
+				{
+					$insererPseudo = $bdd->prepare('UPDATE members SET pseudo = :pseudo WHERE id = :id');
+					$insererPseudo->execute([':pseudo'=>$nouveauPseudo, ':id'=>$_SESSION['id']]);
+					$_SESSION['pseudo'] = $nouveauPseudo ;
+					header('Location: accueil.php ');	
+				}
+				else
+				{
+				$erreur = ' Ce pseudo est déjà pris';
+				}
+			}
+			else
+			{
+				$erreur = ' Le pseudo ne doit pas dépasser 30 caractères';
+			}	
+	}
+	
+	if (isset($_POST['formulaire_edition']) AND !empty($_POST['motdepasse_actuel']) AND !empty($_POST['nouveau_motdepasse']))
+	{
+		$motDePasseVerification = sha1($_POST['motdepasse_actuel']);
+		$nouveauMotDePasse = sha1($_POST['nouveau_motdepasse']);
+		if($motDePasseVerification != $nouveauMotDePasse)
+		{	
+			$motDePasselength = strlen($nouveauMotDePasse);
+			if($motDePasselength >= 10)
+			{
+				$requeteMotDePasse = $bdd->prepare('SELECT * FROM members WHERE id = ' . $_SESSION['id'] . '  AND motdepasse = :motdepasse');
+				$requeteMotDePasse->execute(['motdepasse'=>$motDePasseVerification]);
+				$motDePasseExiste = $requeteMotDePasse->rowCount();
+
+				if ($motDePasseExiste === 1)
+				{	
+					$insererMotDePasse = $bdd->prepare('UPDATE members SET motdepasse = :motdepasse WHERE id = :id');
+					$insererMotDePasse->execute(['motdepasse'=>$nouveauMotDePasse, ':id'=>$_SESSION['id']]);
+					header('Location: accueil.php ');
+				}
+				else
+				{
+					$erreur = ' mot de passe incorrect!';
+				}
+			}
+			else
+			{
+				$erreur = ' Le mot de passe doit comporter au moins 10 caractères';
+			}
+		}
+		else
+		{
+			$erreur = 'mots de passe identiques';
+		}
+	}
+	
+	if (isset($_POST['nouvelle_question']) AND !empty($_POST['nouvelle_question']) AND $_POST['nouvelle_question'] != $_SESSION['question']) 
+	{
+		$nouvelleQuestion = htmlspecialchars($_POST['nouvelle_question']);
+		$insererQuestion = $bdd->prepare('UPDATE members SET question = :question WHERE id = :id');
+		$insererQuestion->execute([':question'=>$nouvelleQuestion, ':id'=>$_SESSION['id']]);
+		$_SESSION['question'] = $nouvelleQuestion;
+		header('Location: accueil.php ');
+		
+	}
+	if (isset($_POST['nouvelle_reponse']) AND !empty($_POST['nouvelle_reponse']) AND $_POST['nouvelle_reponse'] != $_SESSION['reponse']) 
+	{
+		$nouvelleReponse = htmlspecialchars($_POST['nouvelle_reponse']);
+		$insererReponse = $bdd->prepare('UPDATE members SET reponse = :reponse WHERE id = :id');
+		$insererReponse->execute(['reponse'=>$nouvelleReponse, 'id'=>$_SESSION['id']]);
+		$_SESSION['reponse'] = $nouvelleReponse;
+		header('Location: accueil.php ');
+	}
 ?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -51,6 +161,13 @@ if (isset($_SESSION['id']))
 						<input type="submit" name="formulaire_edition" value="Mettre à jour">
 						
 					</form><br />
+
+					<?php
+						if (isset($erreur)) 
+						{
+							echo '<p> <font color="red"> ' . $erreur . ' </p> ';
+						}
+					?>
 
 				</div>
 			</div>
