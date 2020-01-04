3c56949eb1fe4ae807046b40d049dc01f18adf34
FatherKarras333 <florent.victorin@icloud.com>
2020-01-04 10:38:01 +0100

    Création de la page acteur CDE

---
+++ CDE.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+
+if(isset($_SESSION['id']))
+{
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="styles.css">
+		<meta charset="utf-8">
+		<title> GBAF </title>
+	</head>
+
+	<body>
+		
+			<?php include('includes/header_connecte.php'); ?>
+
+			<section class="details">
+				<p><img src="images/CDE_full.png" alt="logo cde"></p>
+				<h2> CDE ( Chambre des entrepreneurs) </h2>
+				<p class="align">La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation. 
+				   Son président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.</p>
+			</section>	
+
+			<section id="comments">
+				<div class="entete_commentaire">
+					<h3> Commentaires</h3>
+					<div class="likes">
+						<a class="bouton_commentaire" href="#"> Nouveau commentaire  </a> 
+						<a href="#"><img src="images/like24px.png"> </a>
+						<a href="#"><img src="images/dislike24px.png"> </a>
+					</div>
+				</div>
+				
+			</section>
+			<?php include('includes/footer.php'); ?>
+	</body>
+</html>	
+<?php
+}
+else
+{
+	header("Location: index.php");
+}
+?>
