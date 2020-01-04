5991f464e53577104227de3c9c43494a8323225a
FatherKarras333 <florent.victorin@icloud.com>
2020-01-04 15:33:50 +0100

    Création des mentions légales

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer>
 	
-	<p class="marge"> |<a href="#" class="texte_footer">| Mentions légales |</a> <a href="mailto:florent.victorin@icloud.com" class="texte_footer"> Contact |</a></p>
+	<p class="marge"> |<a href="mentionslegales.html" class="texte_footer">| Mentions légales |</a> <a href="mailto:florent.victorin@icloud.com" class="texte_footer"> Contact |</a></p>
 	<p class="oc"> Site réalisé dans le cadre d'une formation Openclassrooms</p>
 				
 </footer>
