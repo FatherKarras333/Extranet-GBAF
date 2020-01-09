7366c4b86d95858771c41b01ec6d2d079c88764c
FatherKarras <florent.victorin@icloud.com>
2020-01-09 11:56:04 +0100

    Création d'un include du head pour la page d'accueil et les pages acteurs

---
+++ source/includes/head_connect.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=0.8">
+		<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="styles.css">
+		<title> GBAF </title>
+	</head>
