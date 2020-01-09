802e9665948a0cbf584c858fd8de276f74b65277
FatherKarras <florent.victorin@icloud.com>
2020-01-09 11:47:57 +0100

    Création d'un include du head pour les pages contenant un formulaire

---
+++ source/includes/head_deconnecte.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=0.8">
+		<title> GBAF </title>
+		<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="styles.css">
+		<link rel="stylesheet" type="text/css" href="form.css">
+	</head>
